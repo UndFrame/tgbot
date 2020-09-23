030bbe6659a4941437d5409123a9ea7ed5199220
masonvqnx <masonvqnx@yandex.ru>
2020-09-23 21:30:36 +0300

    ...

--- src/main/java/ru/masonvqnx/tgbot/TBot.java
+++ src/main/java/ru/masonvqnx/tgbot/TBot.java
@@ -3,7 +3,7 @@ package ru.masonvqnx.tgbot;
 public class TBot {
 
     public static void main(String[] args) {
-        System.out.println("UNDFRAME <3");
+        System.out.println("undfрэм");
     }
 
 }
